4841ffdf2570aafec8a731fef4e3b81995ddf995
BuildTools <unconfigured@null.spigotmc.org>
2020-06-04 03:10:40 +0300

    eslint

--- bot.js
+++ bot.js
@@ -1,18 +1,26 @@
-'use strict'
+'use strict';
 
 const { Telegraf } = require('telegraf');
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf('1145790511:AAFx6ggzoezwJBr7WJ4DLGSGsEkmhM00DK4');
 const axios = require('axios');
 
-//ДОБАВИТЬ ДЕЙСТВИЯ КНОПКЕ ТАБЛЕТКИ, СДЕЛАТЬ ТАК, ЧТО Б ДАННЫЕ МОЖНО БЫЛО ИЗМЕНЯТЬ ЧЕРЕЗ ЛОГИ
-//автоматизировать выдачу анализов из бота: парсить сайт или подконектить бд и начать с ней работу в этом боте(что б бот считывал данные с My SQL и передавал их, а не кидал скриншоты анализов)
-//автоматизировать всю эту байду с анализами(сделать так, что б после сдачи, я сразу смог посмотреть эти данные в боте, задача не из лёгких...)
-//сделать заполнение данных про таблетки(что б в личной группе с ботом, я мог писать какое к-во таблеток я пью на данный момент)
-//исправить генерацию рандомных фактов
-//изменить структуру кода
+/* -ДОБАВИТЬ ДЕЙСТВИЯ КНОПКЕ ТАБЛЕТКИ,
+СДЕЛАТЬ ТАК, ЧТО Б ДАННЫЕ МОЖНО БЫЛО ИЗМЕНЯТЬ ЧЕРЕЗ ЛОГИ.
+-автоматизировать выдачу анализов из бота:
+парсить сайт или подконектить бд и начать с ней работу в этом боте
+(что б бот считывал данные с My SQL и передавал их,
+а не кидал скриншоты анализов)
+-автоматизировать всю эту байду с анализами
+(сделать так, что б после сдачи,
+я сразу смог посмотреть эти данные в боте, задача не из лёгких...)
+-сделать заполнение данных про таблетки
+-исправить генерацию рандомных фактов*/
 
 
-//функция которая отвечает за мою личную группу бота с логами(он туда отправялет всё, что пишут ему другие юзеры, я в этой группе вижу их ник, и что они написали, если же они нажали кнопку, я вижу что они её нажали)
+/*функция которая отвечает за мою личную группу бота с логами
+(он туда отправялет всё, что пишут ему другие юзеры,
+я в этой группе вижу их ник,
+и что они написали, если же они нажали кнопку, я вижу что они её нажали)*/
 bot.use(async (ctx, next) => {
   // мои бренные попытки парсить сайт с анализами
   // await axios.get('https://patient-docs.com/')
@@ -23,21 +31,21 @@ bot.use(async (ctx, next) => {
   //         // console.log(response.headers);
   //         // console.log(response.config);
   //     });
-  if (ctx.updateSubTypes[0] == 'text') {
+  if (ctx.updateSubTypes[0] === 'text') {
     bot.telegram.sendMessage(-498566951, ctx.from.username + ' написал: ' + ctx.message.text);
-  } else if (ctx.updateType == 'callback_query') {
+  } else if (ctx.updateType === 'callback_query') {
     bot.telegram.sendMessage(-498566951, ctx.from.username + ' тыкнул кнопку');
   } else {
     bot.telegram.sendMessage(-498566951, ctx.from.username + ' написал: ' + ctx.updateSubTypes[0]);
   }
   next();
-})
+});
 
 //функция которая отвечает за первое сообщение, это пишет бот, когда Вы пишите /start
 function sendStartMessage(ctx) {
-  let startMessage = `Здравствуй, этот бот служит личным дневником Дани, в нём записаны все анализы и количество таблеток которое он выпил на протяжении какого то времени`;
-  if (ctx.from.username === "ddynikov") {
-    startMessage = `Привет хозяин`;
+  let startMessage = 'Здравствуй, этот бот служит личным дневником Дани, в нём записаны все анализы и количество таблеток которое он выпил на протяжении какого то времени';
+  if (ctx.from.username === 'ddynikov') {
+    startMessage = 'Привет хозяин';
 
   }
   bot.telegram.sendMessage(ctx.chat.id, startMessage,
@@ -55,22 +63,22 @@ function sendStartMessage(ctx) {
           ]
         ]
       }
-    })
+    });
 }
 
 //кривая генерация выплёвывания рандомных фактов
 const getData = async () => {
 
-  const json = await axios('https://spreadsheets.google.com/feeds/cells/1JBUpCCPwOpUOFyPCmeqhUZmbvDoTc_Hytb52RRv_vhE/1/public/full?alt=json')
-  let data = json.data.feed.entry;
+  const json = await axios('https://spreadsheets.google.com/feeds/cells/1JBUpCCPwOpUOFyPCmeqhUZmbvDoTc_Hytb52RRv_vhE/1/public/full?alt=json');
+  const data = json.data.feed.entry;
   const factStore = [];
   data.forEach(item => {
     factStore.push({
       row: item.gs$cell.row,
       col: item.gs$cell.col,
       val: item.gs$cell.inputValue,
-    })
-  })
+    });
+  });
   return (factStore);
 };
 
@@ -81,7 +89,7 @@ const getData = async () => {
 
 //появления новых кнопок, при нажатии кнопки "Я эндокринолог"
 bot.action('doc', ctx => {
-  let infoMessage = `Узнать информацию. Выберите, что хотите узнать`;
+  const infoMessage = 'Узнать информацию. Выберите, что хотите узнать';
   ctx.deleteMessage();
   bot.telegram.sendMessage(ctx.chat.id, infoMessage, {
     reply_markup: {
@@ -98,13 +106,21 @@ bot.action('doc', ctx => {
       ]
     }
 
-  })
+  });
 
-})
+});
+
+// bot.command('pills', ctx => {
+
+// })
+
+// bot.action('pills', ctx => {
+
+// })
 
 //появление нового диалогового окна с кнопками после нажатия кнопки "Прошлые анализы"
-bot.action('analyzes', (ctx) => {
-  let infoMessage = `Какая дата сдачи Вас интересует?`;
+bot.action('analyzes', ctx => {
+  const infoMessage = 'Какая дата сдачи Вас интересует?';
   ctx.deleteMessage();
   bot.telegram.sendMessage(ctx.chat.id, infoMessage, {
     reply_markup: {
@@ -130,13 +146,14 @@ bot.action('analyzes', (ctx) => {
       ]
     }
 
-  })
+  });
 
-})
+});
 
 //появление нового диалогового окна с кнопками после нажатия кнопки "Я не доктор"
-bot.action('user', (ctx) => {
-  let infoMessage = `Вся суть этого бота в том, что он хранит в себе данные анализов и количество таблеток которые Даня выпил на протяжении, но ты можешь прочитать рандомный факт х)`;
+bot.action('user', ctx => {
+  const infoMessage = 'Вся суть этого бота в том, что он хранит в себе данные анализов и количество таблеток которые Даня пьёт на данный момент, но ты можешь прочитать рандомный факт х)';
+  ctx.deleteMessage();
   bot.telegram.sendMessage(ctx.chat.id, infoMessage, {
     reply_markup: {
       inline_keyboard: [
@@ -156,10 +173,11 @@ bot.action('user', (ctx) => {
       ]
     }
 
-  })
-})
+  });
+});
 
-//кривой выводрандомных фактов, пока что их пять, эта часть кода в ранней стадии разработки, работает через одно место
+/*кривой выводрандомных фактов, пока что их пять,
+эта часть кода в ранней стадии разработки, работает через одно место*/
 
 
 
@@ -168,8 +186,8 @@ bot.action('fact', async ctx => {
   factStore.shift();
 
 
-  let k = Math.floor(Math.random() * factStore.length);
-  let fact = factStore[k];
+  const k = Math.floor(Math.random() * factStore.length);
+  const fact = factStore[k];
   const message = `${fact.val}`;
 
   ctx.reply(message);
@@ -213,6 +231,7 @@ for (let i = 1; i <= 4; i++) {
   let date = initial.first;
 
   bot.action(`gor${i}`, ctx => {
+    ctx.deleteMessage();
     bot.telegram.sendPhoto(ctx.chat.id, {
       source: `analyzes/gor${date}.jpg`
     });
@@ -233,5 +252,6 @@ for (let i = 1; i <= 4; i++) {
 // });
 
 
-bot.telegram.setWebhook(`${process.env.BOT_URL}/bot${process.env.BOT_TOKEN}`);
-bot.startWebhook(`/bot${process.env.BOT_TOKEN}`, null, process.env.PORT);
+//bot.telegram.setWebhook(`${process.env.BOT_URL}/bot${process.env.BOT_TOKEN}`);
+//bot.startWebhook(`/bot${process.env.BOT_TOKEN}`, null, process.env.PORT);
+bot.launch();

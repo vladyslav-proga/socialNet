5a10b571533e128425eb80c0f93303ffdda6d622
17etro <petrpetryk2016@gmail.com>
2020-09-09 22:42:10 +0300

    create layout and simple navigation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+const app = (
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+ReactDOM.render( 
+  app, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change

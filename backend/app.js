09e155b21c53a8ed8bbacd3f143992e5743f5b83
17etro <petrpetryk2016@gmail.com>
2020-09-27 18:08:06 +0300

    create controllers and routes

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
-const db = require('./util/database');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const User = require('./models/users')
+
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -10,32 +10,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.post('/auth/signup', (req, res, next) => {
-    const fname = req.body.fname;
-    const lname = req.body.lname;
-    const email = req.body.email;
-    const pass = req.body.password;
-    console.log(fname, lname, email, pass)
-    const user = new User(
-        fname,
-        lname,
-        email,
-        pass
-    ).save();
-})
-
-db.execute(`select * from users`)
-    .then(result => {
-        console.log(result[0]);
-    })
-    .then(() => {
-        console.log('Подключение закрыто')
-        db.end()
-    })
-    .catch(err => {
-        console.log(err);
-    });
-
+app.use('/auth', authRoutes);
 
 app.listen(5000);

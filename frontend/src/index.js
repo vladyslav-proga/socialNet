dc2ecfd5142fe2da637c7f93defe1a2742d9ecf0
BuildTools <unconfigured@null.spigotmc.org>
2020-11-13 15:29:32 +0200

    testtest

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,10 +10,10 @@ import * as serviceWorker from './serviceWorker';
 
 import authReducer from './store/reducers/auth';
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = compose;
 
 const rootReducer = combineReducers({
-    auth: authReducer
+  auth: authReducer
 });
 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
@@ -26,8 +26,8 @@ const app = (
   </Provider>
 );
 
-ReactDOM.render( 
-    app
+ReactDOM.render(
+  app
   , document.getElementById('root')
 );
 
